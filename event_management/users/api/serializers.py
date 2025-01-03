e20752330633a7dcf588ab592f4c3aafbdc58cb4
Mustafa Abbas <mustufa.abbas110@gmail.com>
2025-01-03 18:41:27 +0500

    feat(auth): configure user model (Add mobile_number field) and configure authentication system (Develop Authentication APIs)

--- event_management/users/api/serializers.py
+++ event_management/users/api/serializers.py
@@ -6,8 +6,23 @@ from event_management.users.models import User
 class UserSerializer(serializers.ModelSerializer[User]):
     class Meta:
         model = User
-        fields = ["username", "name", "url"]
+        fields = ["pk", "username", "name", "mobile_number", "url"]
 
         extra_kwargs = {
             "url": {"view_name": "api:user-detail", "lookup_field": "username"},
         }
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "password"]
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            password=validated_data["password"],
+        )
+        return user
